887b166b5ad773db080ade49232531395e3ca40e
mervynzhang <sads@live.com>
2018-07-31 22:37:02 +0800

    add data

--- src/main/java/org/mz/Ccgui.java
+++ src/main/java/org/mz/Ccgui.java
@@ -1,49 +1,75 @@
 package org.mz;
  
+
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.*;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
- 
-public class Ccgui extends Application {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage stage) {
-        Scene scene = new Scene(new Group());
-        stage.setTitle("Table View Sample");
-        stage.setWidth(300);
-        stage.setHeight(500);
- 
-        final Label label = new Label("Address Book");
-        label.setFont(new Font("Arial", 20));
- 
-        TableView table = new TableView();
-		table.setEditable(true);
- 
-        TableColumn firstNameCol = new TableColumn("First Name");
-        TableColumn lastNameCol = new TableColumn("Last Name");
-        TableColumn emailCol = new TableColumn("Email");
-        
-        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
- 
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label, table);
- 
-        ((Group) scene.getRoot()).getChildren().addAll(vbox);
- 
+
+import java.util.*;
+
+public class Ccgui extends Application {    
+    private static final int N_COLS = 5;
+    private static final int N_ROWS = 1000;
+
+    public void start(Stage stage) throws Exception {
+        TestDataGenerator dataGenerator = new TestDataGenerator();
+
+        TableView<ObservableList<String>> tableView = new TableView<ObservableList<String>>();
+
+        // add columns
+        List<String> columnNames = dataGenerator.getNext(N_COLS);
+        for (int i = 0; i < columnNames.size(); i++) {
+            final int finalIdx = i;
+            TableColumn<ObservableList<String>, String> column = new TableColumn<ObservableList<String>, String>(
+                    columnNames.get(i)
+            );
+            column.setCellValueFactory(param ->
+                    new ReadOnlyObjectWrapper<>(param.getValue().get(finalIdx))
+            );
+            tableView.getColumns().add(column);
+        }
+
+        // add data
+        for (int i = 0; i < N_ROWS; i++) {
+            tableView.getItems().add(
+                    FXCollections.observableArrayList(
+                            dataGenerator.getNext(N_COLS)
+                    )
+            );
+        }
+
+        tableView.setPrefHeight(200);
+
+        Scene scene = new Scene(tableView);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static class TestDataGenerator {
+        private static final String[] LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus cursus diam ac blandit. Ut ultrices lacus et mattis laoreet. Morbi vehicula tincidunt eros lobortis varius. Nam quis tortor commodo, vehicula ante vitae, sagittis enim. Vivamus mollis placerat leo non pellentesque. Nam blandit, odio quis facilisis posuere, mauris elit tincidunt ante, ut eleifend augue neque dictum diam. Curabitur sed lacus eget dolor laoreet cursus ut cursus elit. Phasellus quis interdum lorem, eget efficitur enim. Curabitur commodo, est ut scelerisque aliquet, urna velit tincidunt massa, tristique varius mi neque et velit. In condimentum quis nisi et ultricies. Nunc posuere felis a velit dictum suscipit ac non nisl. Pellentesque eleifend, purus vel consequat facilisis, sapien lacus rutrum eros, quis finibus lacus magna eget est. Nullam eros nisl, sodales et luctus at, lobortis at sem.".split(" ");
+
+        private int curWord = 0;
+
+        List<String> getNext(int nWords) {
+            List<String> words = new ArrayList<String>();
+
+            for (int i = 0; i < nWords; i++) {
+                if (curWord == Integer.MAX_VALUE) {
+                    curWord = 0;
+                }
+
+                words.add(LOREM[curWord % LOREM.length]);
+                curWord++;
+            }
+
+            return words;
+        }
     }
 }
